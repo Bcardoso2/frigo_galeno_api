ec779b8907687b4756dccacee50ff9f6307210fd
Bcardoso2 <38514089+Bcardoso2@users.noreply.github.com>
2025-04-30 17:36:04 -0300

    Update estoqueAnimalRoutes.js

--- routes/estoqueAnimalRoutes.js
+++ routes/estoqueAnimalRoutes.js
@@ -1,21 +1,25 @@
 // routes/estoqueAnimalRoutes.js
 const express = require('express');
 const router = express.Router();
-const EstoqueAnimalController = require('../controllers/estoqueAnimalController');
+const { 
+  adicionarEstoqueAnimal, 
+  listarEstoqueAnimal, 
+  getEstoqueAnimalPorId, 
+  atualizarEstoqueAnimal, 
+  excluirEstoqueAnimal 
+} = require('../controllers/estoqueAnimalController');
+const { protect, admin } = require('../middleware/authMiddleware');
 
-// Rota para listar todos os registros
-router.get('/', EstoqueAnimalController.index);
+// Rotas protegidas (requerem autenticação)
+router
+  .route('/')
+  .post(protect, admin, adicionarEstoqueAnimal)
+  .get(protect, listarEstoqueAnimal);
 
-// Rota para exibir um registro específico
-router.get('/:id', EstoqueAnimalController.show);
-
-// Rota para criar um novo registro
-router.post('/', EstoqueAnimalController.store);
-
-// Rota para atualizar um registro
-router.put('/:id', EstoqueAnimalController.update);
-
-// Rota para excluir um registro
-router.delete('/:id', EstoqueAnimalController.destroy);
+router
+  .route('/:id')
+  .get(protect, getEstoqueAnimalPorId)
+  .put(protect, admin, atualizarEstoqueAnimal)
+  .delete(protect, admin, excluirEstoqueAnimal);
 
 module.exports = router;
